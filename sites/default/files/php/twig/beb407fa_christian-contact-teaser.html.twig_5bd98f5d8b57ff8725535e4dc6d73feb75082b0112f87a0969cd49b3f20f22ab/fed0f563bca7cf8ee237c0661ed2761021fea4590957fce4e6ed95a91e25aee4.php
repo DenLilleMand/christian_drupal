be1d2c5955c1d0dfa02182d6e13bd2258e0752c7
denlillemand <mattinielsen5@hotmail.com>
2015-12-21 02:01:00 +0100

    something something

---
+++ sites/default/files/php/twig/beb407fa_christian-contact-teaser.html.twig_5bd98f5d8b57ff8725535e4dc6d73feb75082b0112f87a0969cd49b3f20f22ab/fed0f563bca7cf8ee237c0661ed2761021fea4590957fce4e6ed95a91e25aee4.php
@@ -0,0 +1,104 @@
+<?php
+
+/* modules/custom/christian/templates/christian-contact-teaser.html.twig */
+class __TwigTemplate_cdda8fd1094a907775554e1a406c18f4094297723c0c15a1658c6dd31b4ce614 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<div class=\"col-lg-4\">
+    <img class=\"img-circle\" src=";
+        // line 2
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["image"]) ? $context["image"] : null), "html", null, true));
+        echo " alt=\"Murer\" width=\"140\" height=\"140\"/>
+
+    <h2>
+        Kontakt os på
+    </h2>
+
+    <p>
+        <div class=\"input-group-addon\">
+            Telefon
+        </div>
+        <div class=\"form-control\">
+            30 13 38 34
+        </div>
+    </p>
+    <p>
+        <a class=\"btn btn-secondary contact-teaser-button\" href=\"#christian-contact-form-id\" role=\"button\">
+            Eller lad os kontakte dig &raquo;
+        </a>
+    </p>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/custom/christian/templates/christian-contact-teaser.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  46 => 2,  43 => 1,);
+    }
+}
+/* <div class="col-lg-4">*/
+/*     <img class="img-circle" src={{ image }} alt="Murer" width="140" height="140"/>*/
+/* */
+/*     <h2>*/
+/*         Kontakt os på*/
+/*     </h2>*/
+/* */
+/*     <p>*/
+/*         <div class="input-group-addon">*/
+/*             Telefon*/
+/*         </div>*/
+/*         <div class="form-control">*/
+/*             30 13 38 34*/
+/*         </div>*/
+/*     </p>*/
+/*     <p>*/
+/*         <a class="btn btn-secondary contact-teaser-button" href="#christian-contact-form-id" role="button">*/
+/*             Eller lad os kontakte dig &raquo;*/
+/*         </a>*/
+/*     </p>*/
+/* </div>*/
